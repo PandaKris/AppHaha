840cc5d30a1010f8d7d046f970073ae0247c1f26
PandaKris <kristantosean@gmail.com>
2023-06-21 14:54:55 +0700

    nambah onboarding page

---
+++ AppHaha/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  AppHaha
+//
+//  Created by Kristanto Sean on 21/06/23.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text("Ini Onboarding")
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
