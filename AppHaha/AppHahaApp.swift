d51a1e934a0decf064ccfe07c03d65a9d9957fbd
PandaKris <kristantosean@gmail.com>
2023-06-21 14:20:17 +0700

    Initial Commit

---
+++ AppHaha/AppHahaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppHahaApp.swift
+//  AppHaha
+//
+//  Created by Kristanto Sean on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AppHahaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
